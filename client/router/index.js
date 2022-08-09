05db669f4a6890849f6059a7029f0adbc375d909
matanjos <matheusmartins1000@gmail.com>
2022-08-09 13:11:03 -0300

    Commit new

--- client/router/index.js
+++ client/router/index.js
@@ -3,14 +3,13 @@ import SobreComponent from '../src/components/SobreComponent.vue'
 import HomeComponent from '../src/components/HomeComponent.vue'
 import FuturoComponent from '../src/components/FuturoComponent.vue'
 import PassadoComponent from '../src/components/PassadoComponent.vue'
-import SimuladoComponent from '../src/components/PassadoComponent.vue'
+const path = require('path');
 
 const routes = [
-    { path: '/',  name: "Home" ,component: HomeComponent, children:[] },
-    { path: '/simulpassado',name: "Simular Passado" , component: PassadoComponent},
-    { path: '/simulfuturo',name: "Simular Futuro" , component: FuturoComponent },
-    { path: '/sobre', name: "Sobre" , component: SobreComponent },
-    { path: '/simulado', name: "Simulado" , component: SimuladoComponent }
+    { path: path.join(__dirname,'/'),  name: "Home" ,component: HomeComponent, children:[] },
+    { path: path.join(__dirname,'/simulpassado'),name: "Simular Passado" , component: PassadoComponent},
+    { path: path.join(__dirname,'/simulfuturo'),name: "Simular Futuro" , component: FuturoComponent },
+    { path: path.join(__dirname,'/sobre'), name: "Sobre" , component: SobreComponent }
 ];
 
 const router = createRouter({
--- client/services/http-common.js
+++ client/services/http-common.js
@@ -3,7 +3,6 @@ export default axios.create({
   //baseURL: "http://localhost:3000",
   baseURL: "https://sim-renda-fixa.herokuapp.com",
   headers: {
-    //"Content-type": "application/json"
   'Content-Type': 'application/x-www-form-urlencoded'
   }
 });
