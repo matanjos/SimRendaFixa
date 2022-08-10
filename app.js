c7615a38d2a380905b5c29f167358a3f993f1faa
matanjos <matheusmartins1000@gmail.com>
2022-08-10 10:53:23 -0300

    htfix

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ mongoose.connect(process.env.DATABASE_URI, {  useNewUrlParser: true, useUnifiedT
 
 
 // middleware
-app.use('/', rotas);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(serveStatic(__dirname + '/client/dist'));
 app.use(history({index: path.join(__dirname,'/client/dist/index.html')}));
 app.use(serveStatic(__dirname + '/client/dist'));
+app.use('/', rotas);
 
 
 
